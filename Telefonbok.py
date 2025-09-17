26df2a1ca0d577fc8c86b2ef97328a4b015fd7b8
“24caratgoldenlabububobatea” <jonathanrasmussen2020@gmail.com>
2025-09-17 15:12:35 +0200

    laget en liste og brukt append.

---
+++ Telefonbok.py
@@ -0,0 +1,17 @@
+telefonbok = []
+
+telefonbok.append({
+    "person1": {
+        "navn": "jonathan",
+        "nummer": "48128944"
+    }
+})
+
+telefonbok.append({
+    "person2": {
+        'navn': 'bob',
+        "nummer": 48104755
+    }
+})
+
+print(telefonbok)
